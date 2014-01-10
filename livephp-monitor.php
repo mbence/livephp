8fffe2bf752dc58002b943005ad9e5dbb1ee68b0
bence <bence@192.168.0.50>
2014-01-10 15:06:46 +0100

    runtime configuration

--- livephp-monitor.php
+++ livephp-monitor.php
@@ -8,160 +8,162 @@
 
 namespace MBence\LivePHP;
 
-if (!class_exists('LiveMonitor'))
+class LivephpMonitor
 {
-    class LiveMonitor
+    /** list of directories to check for changes */
+    protected $dirs = array('.');
+    /** ignore these files or directories */
+    protected $ignore = array('');
+
+    /** default time limit in seconds */
+    protected $timeLimit = 125;
+    /** Refresh css files without reloading the page */
+    protected $cssOnTheFly = true;
+    /** the time to die */
+    protected $deadLine;
+
+    /**
+     * Constructor
+     */
+    public function __construct($options = array())
     {
-        /** list of directories to check for changes */
-        protected $dirs = array('.');
-        /** ignore these files or directories */
-        protected $ignore = array('');
-
-        /** default time limit in seconds */
-        protected $timeLimit = 125;
-        /** Refresh css files without reloading the page */
-        protected $cssOnTheFly = true;
-        /** the time to die */
-        protected $deadLine;
-
-        /**
-         * Constructor
-         */
-        public function __construct()
-        {
-            // check that $_GET['s'] is a valid unix timestamp
-            if (!empty($_GET['s']) && is_numeric($_GET['s']))
-            {
-                // $_GET['s'] is in millisec, but we need only seconds
-                $start = (int) ($_GET['s'] / 1000);
-
-                $this->headers();
-                $this->setDeadLine();
-                $this->main($start);
-            }
-            else
-            {
-                header('HTTP/1.1 400 Bad Request');
-                die;
-            }
-        }
-
-        /**
-         * Output the no-cache headers
-         */
-        protected function headers()
-        {
-            header('Cache-Control: no-cache, must-revalidate');
-            header('Expires: -1');
-        }
-
-        /**
-         * Sets the time limit if possible
-         */
-        protected function setDeadLine()
-        {
-            // in safe mode there is no way to set the time limit
-            if (!ini_get('safe_mode'))
-            {
-                set_time_limit($this->timeLimit);
-            }
-            // lets check what the actual limit is
-            $limit = ini_get('max_execution_time');
-
-            if (empty($limit) || $limit < 1)
-            {
-                // in case of unsuccesful ini_get, (or unlimited execution), we fall back to the default 30 sec
-                $limit = 30;
-            }
-            // we stop the loop 5 sec befor the time limit, just for sure
-            $this->deadLine = time() + $limit - 5;
-        }
-
-        /**
-         * Main function
-         * @param int $start start date in unix timestamp
-         */
-        protected function main($start)
-        {
-            // clear file status cache
-            clearstatcache();
-            // long polling loop
-            do
-            {
-                // look for the changes every second until the execution time allows it.
-                foreach ($this->dirs as $root)
-                {
-                    $result = $this->checkDir(realpath($root), $start);
-                    if ($result)
-                    {
-                        // if we find modified files in any of the directories, we can skip the rest
-                        echo json_encode($result);
-
-                        die;
-                    }
+        // check for runtime options
+        if (!empty($options) && is_array($options)) {
+            foreach ($options as $key => $val) {
+                if (isset($this->$key)) {
+                    $this->$key = $val;
                 }
-
-                sleep(1);
             }
-            while (time() < $this->deadLine);
         }
 
-        /**
-         * A fast (and non-recursive) function to check for modified files in a directory structure
-         *
-         * @param string $root directory path
-         * @param int $start (unix timestamp) to find newer files of
-         * @return bool true (or the modification time of the css file) if modified file found, false otherwise
-         */
-        protected function checkDir($root, $start)
+        // check that $_GET['s'] is a valid unix timestamp
+        if (!empty($_GET['s']) && is_numeric($_GET['s']))
         {
-            $stack[] = $root;
-            // walk through the stack
-            while (!empty($stack))
+            // $_GET['s'] is in millisec, but we need only seconds
+            $start = (int) ($_GET['s'] / 1000);
+
+            $this->headers();
+            $this->setDeadLine();
+            $this->main($start);
+        }
+        else
+        {
+            header('HTTP/1.1 400 Bad Request');
+            die;
+        }
+    }
+
+    /**
+     * Output the no-cache headers
+     */
+    protected function headers()
+    {
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Expires: -1');
+    }
+
+    /**
+     * Sets the time limit if possible
+     */
+    protected function setDeadLine()
+    {
+        // in safe mode there is no way to set the time limit
+        if (!ini_get('safe_mode'))
+        {
+            set_time_limit($this->timeLimit);
+        }
+        // lets check what the actual limit is
+        $limit = ini_get('max_execution_time');
+
+        if (empty($limit) || $limit < 1)
+        {
+            // in case of unsuccesful ini_get, (or unlimited execution), we fall back to the default 30 sec
+            $limit = 30;
+        }
+        // we stop the loop 5 sec befor the time limit, just for sure
+        $this->deadLine = time() + $limit - 5;
+    }
+
+    /**
+     * Main function
+     * @param int $start start date in unix timestamp
+     */
+    protected function main($start)
+    {
+        // clear file status cache
+        clearstatcache();
+        // long polling loop
+        do
+        {
+            // look for the changes every second until the execution time allows it.
+            foreach ($this->dirs as $root)
             {
-                $dir = array_shift($stack);
-                $files = glob($dir . '/*');
-                // make sure that we have an array (glob can return false in some cases)
-                if (!empty($files) && is_array($files))
+                $result = $this->checkDir(realpath($root), $start);
+                if ($result)
                 {
-                    foreach ($files as $file)
+                    // if we find modified files in any of the directories, we can skip the rest
+                    echo json_encode($result);
+
+                    die;
+                }
+            }
+
+            sleep(1);
+        }
+        while (time() < $this->deadLine);
+    }
+
+    /**
+     * A fast (and non-recursive) function to check for modified files in a directory structure
+     *
+     * @param string $root directory path
+     * @param int $start (unix timestamp) to find newer files of
+     * @return bool true (or the modification time of the css file) if modified file found, false otherwise
+     */
+    protected function checkDir($root, $start)
+    {
+        $stack[] = $root;
+        // walk through the stack
+        while (!empty($stack))
+        {
+            $dir = array_shift($stack);
+            $files = glob($dir . '/*');
+            // make sure that we have an array (glob can return false in some cases)
+            if (!empty($files) && is_array($files))
+            {
+                foreach ($files as $file)
+                {
+                    if (empty($this->ignore) || !in_array(basename($file), $this->ignore))
                     {
-                        if (empty($this->ignore) || !in_array(basename($file), $this->ignore))
+                        if (is_dir($file))
                         {
-                            if (is_dir($file))
+                            // we add the directories to the stack to check them later
+                            $stack[] = $file;
+                        }
+                        elseif (is_file($file))
+                        {
+                            // and check the modification times of the files
+                            $mtime = filemtime($file);
+                            if ($mtime && $start < $mtime)
                             {
-                                // we add the directories to the stack to check them later
-                                $stack[] = $file;
-                            }
-                            elseif (is_file($file))
-                            {
-                                // and check the modification times of the files
-                                $mtime = filemtime($file);
-                                if ($mtime && $start < $mtime)
-                                {
-                                    $pinfo = pathinfo($file);
-                                    // return true at the first positive match
-                                    if ($this->cssOnTheFly && $pinfo['extension'] == 'css') {
-                                        // if the file is a css then then we send the whole path back
-                                        return $mtime * 1000;
-                                    }
-                                    else {
-                                        // otherwise return true
-                                        return true;
-                                    }
+                                $pinfo = pathinfo($file);
+                                // return true at the first positive match
+                                if ($this->cssOnTheFly && $pinfo['extension'] == 'css') {
+                                    // if the file is a css then then we send the whole path back
+                                    return $mtime * 1000;
+                                }
+                                else {
+                                    // otherwise return true
+                                    return true;
                                 }
                             }
                         }
-                    } // end foreach
-                }
-            } // end while
+                    }
+                } // end foreach
+            }
+        } // end while
 
-            return false;
-        }
+        return false;
+    }
 
-    } // end LiveMonitor
-
-    new LiveMonitor();
-
-
-} // end class check if
+} // end LivephpMonitor
